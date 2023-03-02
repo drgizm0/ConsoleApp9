876e873fc6c5917238d2587e080597df5b8921e2
Admin <Admin@DESKTOP-9L5JQOO>
2023-03-02 18:59:39 +0800

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+           Snake snake = new Snake();
+            Cow cow = new Cow();
+
+            snake.Speak();
+            cow.Speak();
+
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal goes *brrr*");
+        }
+    }
+    class Snake: Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The snake goes *sssss*");
+        }
+    }
+    class Cow : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The cow goes *mooo*");
+        }
+    }
+}
